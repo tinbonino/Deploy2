9f20fa201d5783660e66be0262d20cac51d45c6a
martin <tinbonino@gmail.com>
2025-01-20 21:52:09 -0300

    first commit

---
+++ src/paginas/Home.jsx
@@ -0,0 +1,13 @@
+
+function Home() {
+  return (
+    <>
+        <h1>Bienvenidos a la clase 9</h1>
+        <h2>hola</h2>
+        <h2>{import.meta.env.VITE_MENSAJE}</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero recusandae sed, eius quod illo eligendi!</p>
+    </>
+  )
+}
+
+export default Home
